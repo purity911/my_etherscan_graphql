c209ea103d113fc66c462372986ed3a858d98953
purity911 <147828090+purity911@users.noreply.github.com>
2023-11-22 13:34:02 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+const { ApolloServer } = require("apollo-server"); // Import Apollo Server
+const { importSchema } = require("graphql-import"); // Import graphql-import to load schema
+const EtherDataSource = require("./datasource/ethDatasource"); // Import custom data source
+
+const typeDefs = importSchema("./schema.graphql"); // Load schema from file
+
+require("dotenv").config(); // Load environment variables
+
+const resolvers = {
+  Query: {
+    // Resolver functions for each query
+    etherBalanceByAddress: (root, _args, { dataSources }) => 
+      dataSources.ethDataSource.etherBalanceByAddress(),
+
+    totalSupplyOfEther: (root, _args, { dataSources }) =>
+      dataSources.ethDataSource.totalSupplyOfEther(),
+
+    latestEthereumPrice: (root, _args, { dataSources }) =>
+      dataSources.ethDataSource.getLatestEthereumPrice(),
+
+    blockConfirmationTime: (root, _args, { dataSources }) =>  
+      dataSources.ethDataSource.getBlockConfirmationTime(),
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  dataSources: () => ({
+    ethDataSource: new EtherDataSource(), 
+  }),
+});
+
+server.timeout = 0; 
+
+server.listen("9000").then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`); 
+});
